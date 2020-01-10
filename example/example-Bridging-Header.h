7ee41d3708125fb0c9c9d1134e82af67d7691a4e
Naveen Thontepu <thontn@amazon.com>
2020-01-10 18:25:22 +0530

    Release 0.0.2

---
+++ example/example-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  example-Bridging-Header.h
+//  example
+//
+//  Created by Thontepu, Naveen on 10/01/20.
+//  Copyright © 2020 Thontepu, Naveen. All rights reserved.
+//
+
+#import <MathOperations/MathOperations.h>
---
+++ example/example/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  example
+//
+//  Created by Thontepu, Naveen on 10/01/20.
+//  Copyright © 2020 Thontepu, Naveen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("the result = ",MathOperations.add(23, andNumber: 423))
+    }
+
+
+}
+
